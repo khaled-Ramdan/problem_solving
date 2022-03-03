efb6afcf15120060af459cf58da3554c0817c29c
Khaled <66100565+khaled-Ramdan@users.noreply.github.com>
2022-03-03 17:01:05 +0200

    Update dollars.cpp

--- DP/dollars.cpp
+++ DP/dollars.cpp
@@ -20,7 +20,7 @@ ll sol(int x, int rem) {//time order O(x*rem*loop(rem))=>O(coins*11*11)
 	if (~ret)return ret;
 	ret = sol(x + 1, rem);//leave it - when you leave it, it tries for different values
 	//and this must be the ret for the sub problem 
-	while (rem>=coins[x])//try as much as you can
+	while (rem >= coins[x])//try as much as you can
 	{
 		ret += sol(x + 1, rem - coins[x]);
 		rem -= coins[x];
@@ -33,7 +33,7 @@ ll sol(int x, int rem) {//time order O(x*rem*loop(rem))=>O(coins*11*11)
 * in first version =>we try leaving current indx and find all ways => then go to next
 * in this version => trace all ways for leaving your self and finding all ways  for yourself
 * when you find your code makes a loop on some indecies => you may can push loop to parameter
-* now you git rid of the loop 
+* now you git rid of the loop
 * O(x*rem)
 */
 ll sol1(int x, int rem) {
@@ -48,12 +48,42 @@ ll sol1(int x, int rem) {
 * dp=> bottom up approach => starts with sub problem and create solution
 * you must be sure that sub problem of x is ready to make process on sub of x+1
 */
+//table method
+ll DP(ll n) {
+	// for any index has zero rem => return 1 (solution of base case subproblem)
+	for (int i = 0;i < 11;i++)dp[i][0] = 1;
+	for (int coin = coins[0];coin < N;coin += 5) {//loop on coins value=> rem
+		for (int i = 0;i < 11;i++) {
+			//give me the value of doing the same operation using other coins
+			//use same value with other coins
+			if (i) dp[i][coin] = dp[i - 1][coin];//leave it => (ret = sol(x + 1, rem))
+			//use the same coin with less value
+			if (coin >= coins[i])dp[i][coin] += dp[i][coin - coins[i]];//sol(x, rem - coins[x])
+		}
+	}
+	return dp[10][n];
+}
+//using rolling techniqe
+ll DP1(ll n) {
+	bool p = 0;
+	dp[0][0] = dp[1][0] = 1;
+	/*swap loop does not affect correctness*/
+	for (int i = 0;i < 11;i++) {
+		p = !p;
+		for (int coin = coins[0];coin < N;coin += 5) {
+			dp[p][coin] = 0;//distroy overlapped layer
+			if (i)dp[p][coin] = dp[!p][coin];//build current layer
+			if (coin >= coins[i])dp[p][coin] += dp[p][coin - coins[i]];
+		}
+	}
+	return dp[!p][n];
+}
 void solve() {
 	float n;
-	memset(dp, -1, sizeof dp);
 	while (scanf("%f", &n))
 	{
-		cout << sol1(0, n*100) << endl;
+		memset(dp, 0, sizeof dp);
+		cout << DP1( n * 100) << endl;
 	}
 }
 int main() {
