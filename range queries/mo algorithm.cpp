a74579376a8345f2d9ca58dbfadfe663be382669
Khaled <66100565+khaled-Ramdan@users.noreply.github.com>
2022-08-07 22:11:04 +0200

    Update mo algorithm.cpp

--- range queries/mo algorithm.cpp
+++ range queries/mo algorithm.cpp
@@ -10,64 +10,64 @@ using namespace std;
 #define W  int g;  cin>>g; while(g--)
 #define FIO freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
 typedef  long long  ll;
-const long long N = 1e5 + 3, Mod = 1e9 + 7, INF = 1e9;
+const long long N = 4e5 + 3, Mod = 1e9 + 7, INF = 1e12;
 const double PI = 3.14159265;
 ll gcd(ll a, ll b) { return a ? gcd(b % a, a) : b; }
 ll lcm(ll a, ll b) { return a * b / gcd(a, b); }
-const int dx[8] = { 0, 0, 1, -1, 1, 1, -1, -1 };
-const int dy[8] = { 1, -1, 0, 0, -1, 1, 1, -1 };
-/*
-time complexity : srqt(n)*(n+q)
-
-*/
-struct query{
+const int dx[9] = { 0, 0, 1, -1, 1, 1, -1, -1,0 };
+const int dy[9] = { 1, -1, 0, 0, -1, 1, 1, -1,0 };
+struct query {
     int l, r, b_idx, q_idx;
-    query() {};
-    query(int l, int r, int q_idx, int sqr) {
-        this->l = l, this->r = r, this->b_idx = l / sqr, this->q_idx = q_idx;
+    query() {
+        l = r = b_idx = q_idx = -1;
+    };
+    query(int ll, int rr, int qqidx, int sqr) {
+        l = ll, r = rr, b_idx = l / sqr, q_idx = qqidx;
     }
     bool operator<(const query& tmp)const {
-        if (tmp.b_idx == this->b_idx)return this->r < tmp.r;
+        if (tmp.b_idx == b_idx)return r < tmp.r;
         return b_idx < tmp.b_idx;
     }
 };
-ll n,m, sqr,res;
-vector<ll>a,ans;
-vector<query>queries;
-void remove(int i) {
-    res -= a[i];
-}
+ll n, m, q, sqr, a[N], ans[N], res, frq[int(1e6 + 1)];
+query queries[N];
 void add(int i) {
-    res += a[i];
+    frq[a[i]]++;
+    if (frq[a[i]] == 1)
+        res++;
+}
+void remove(int i) {
+    frq[a[i]]--;
+    if (frq[a[i]] == 0)
+        res--;
 }
 void process() {
-    sort(queries.begin(), queries.end());
+    sort(queries, queries + m);
     int curL = 1, curR = 0;
-    for (query& q : queries) {
+    for (auto& q : queries) {
         while (curL < q.l)remove(curL++);
         while (curL > q.l)add(--curL);
-        while (curR < q.r)add(++curL);
-        while (curR > q.r)remove(curL--);
+        while (curR < q.r)add(++curR);
+        while (curR > q.r)remove(curR--);
         ans[q.q_idx] = res;
     }
 }
-void solve() {
-    cin >> n;
-    sqr = (int)ceil(sqrt(n));
-    a = vector<ll>(n);
-    for (ll& i : a)
-        cin >> i;
+void solve(){
+    cin >> n;       sqr = (int)ceil(sqrt(n));
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
     cin >> m;
-    ans = vector<ll>(m);
     for (int i = 0; i < m; i++) {
-        int l, r;
-        cin >> l >> r;
-        --l, --r;
-        queries[i] = query(l, r, i, sqr);
-   }
+        int left, right;
+        cin >> left >> right;
+        queries[i] = query(--left, --right, i, sqr);
+    }
+    process();
+    for (int i = 0; i < m; i++)
+        cout << ans[i] << endl;
 }
 int main() {
     IO;
-    W solve();
+    solve();
     return 0;
 }
