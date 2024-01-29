59a29645437703a4005212e2fe8bc97e466d7532
Khaled Ramadan <66100565+khaled-Ramdan@users.noreply.github.com>
2024-01-29 13:57:14 +0200

    Update numbr_theory.cpp

--- number theory/numbr_theory.cpp
+++ number theory/numbr_theory.cpp
@@ -207,3 +207,35 @@ map<ll, ll> factors(ll n) {
 	}
 	return ret;
 }
+///.............. extended GCD
+/*
+    representation of gcd(a, b) in form of a*x + b *y = gcd(a, b)
+    x => is the multiplication inverse of a % b
+*/
+int extended_gcd(int a, int b, int &x, int &y)
+{
+    x = 1, y = 0;
+    int x1 = 0, y1 = 1, a1 = a, b1 = b;
+    while (b1)
+    {
+        int q = a1 / b1;
+        tie(x, x1) = make_tuple(x1, x - q * x1);
+        tie(y, y1) = make_tuple(y1, y - q * y1);
+        tie(a1, b1) = make_tuple(b1, a1 - q * b1);
+    }
+    return a1;
+}
+int rec_gcd(int a, int b, int &x, int &y)
+{
+    if (b == 0)
+    {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = rec_gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
