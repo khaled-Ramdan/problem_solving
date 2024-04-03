7848c731f28ab690700ce69de2ce494a5755dbb3
Khaled Ramadan <66100565+khaled-Ramdan@users.noreply.github.com>
2024-04-03 15:39:28 +0200

    Update suffix_array.cpp

--- String-Algorithms/suffix_array.cpp
+++ String-Algorithms/suffix_array.cpp
@@ -13,15 +13,27 @@ private:
     {
         this->s = str;
         this->s += '$';
-        // but $ before n = s.size
+        // put $ before n = s.size
         n = s.size();
-        suffix = c = vector<int>(n);
+        KtoL = suffix = vector<int>(n);
+        last_k = 0; // last index in suffix array
+        while ((1 << last_k) < n)
+            last_k++;
+        c = vector<vector<int>>(last_k + 1, vector<int>(n));
+
+        for (int i = 0, j = 0; i < n; i++)
+        {
+            while ((1 << (j + 1)) <= i)
+                j++;
+            KtoL[i] = j;
+        }
     }
 
 public:
     string s;
-    int n;
-    vector<int> suffix, c, lcp;
+    int n, last_k = 0;
+    vector<int> suffix, lcp, KtoL;
+    vector<vector<int>> c;
     suffix_array() {}
     suffix_array(string s)
     {
@@ -29,6 +41,7 @@ public:
     }
     void build()
     {
+        int k = 0;
         { // k = 0
             vector<pair<char, int>> a(n);
             for (int i = 0; i < n; i++)
@@ -43,28 +56,25 @@ public:
             // a string which is equal to previous one have same class
             // otherwise take next class
 
-            c[suffix[0]] = 0;
+            c[k][suffix[0]] = 0;
             for (int i = 1; i < n; i++)
-                c[suffix[i]] = c[suffix[i - 1]] + (a[i].first != a[i - 1].first);
+                c[k][suffix[i]] = c[k][suffix[i - 1]] + (a[i].first != a[i - 1].first);
         }
-        int k = 0;
         while ((1 << k) < n)
         {                               // k -> k + 1
             for (int i = 0; i < n; i++) // shift to the righ by 1 << k to be sorted by second half
                 suffix[i] = (suffix[i] - (1 << k) + n) % n;
 
-            count_sort(suffix, c);
+            count_sort(suffix, c[k]);
 
-            vector<int> c_new(n);
-            c_new[suffix[0]] = 0;
+            c[k + 1][suffix[0]] = 0;
             for (int i = 1; i < n; i++)
             {
-                pair<int, int> prv = {c[suffix[i - 1]], c[(suffix[i - 1] + (1 << k)) % n]};
-                pair<int, int> cur = {c[suffix[i]], c[(suffix[i] + (1 << k)) % n]};
+                pair<int, int> prv = {c[k][suffix[i - 1]], c[k][(suffix[i - 1] + (1 << k)) % n]};
+                pair<int, int> cur = {c[k][suffix[i]], c[k][(suffix[i] + (1 << k)) % n]};
 
-                c_new[suffix[i]] = c_new[suffix[i - 1]] + (cur != prv);
+                c[k + 1][suffix[i]] = c[k + 1][suffix[i - 1]] + (cur != prv);
             }
-            c = c_new;
             k++;
         }
     }
@@ -89,12 +99,12 @@ public:
         suffix = suffix_new;
     }
     void lcp_build()
-    { // get the longest common prefix of consecutive suffixes in O(n)
+    { // get the longest common prefix of consecutive suffixes in O(n) between me and before me
         lcp = vector<int>(n);
         int k = 0;
         for (int i = 0; i < n - 1; i++)
         {
-            int pi = c[i];
+            int pi = c[last_k][i];
             int j = suffix[pi - 1]; // lcp[i] = lcp(s[i.. ], s[j...])
             while (s[(i + k) % n] == s[(j + k) % n])
                 k++;
@@ -105,7 +115,7 @@ public:
     // lcp[i] = LCP(i, i - 1) , lcp(i, j) = Min(lcp[i...j]) => use sparse table or segment tree
 
     ll different_substrings_count()
-    {   // returns the number of different substrings in a string :: require => build(), lcp_build()
+    { // returns the number of different substrings in a string :: require => build(), lcp_build()
         ll ans = 0;
 
         for (int i = 1; i < n; i++)
@@ -137,14 +147,53 @@ public:
         }
         return str.substr(pos, len);
     }
+    // compare two strings from statring positions i, j with length l where k is the greatest such that 2**k <= l
+    int compare(int i, int j, int l) // requires build()
+    {
+        pair<int, int> a = {c[KtoL[l]][i], c[KtoL[l]][(i + l - (1 << KtoL[l])) % n]};
+        pair<int, int> b = {c[KtoL[l]][j], c[KtoL[l]][(j + l - (1 << KtoL[l])) % n]};
+        if (a == b)
+            return 0;
+        return a < b ? -1 : 1;
+    }
+    // compare two strings s[a.fr ... a.sec] , s[b.fr ... b.sec] requires build()
+    // make sure using 0 based pairs
+    bool comp(const pair<int, int> &a, const pair<int, int> &b)
+    {
+        int l1 = a.second - a.first + 1, l2 = b.second - b.first + 1;
+        int l = min(l1, l2);
+        int res = compare(a.first, b.first, l);
+        if (res != 0)
+            return res == -1;
+
+        if (l1 == l2)
+            return a < b;
+
+        return l1 < l2;
+    }
 };
+
 void solve()
 {
-    string s, t;
-    cin >> s >> t;
-    string str = s + "#" + t;
+    string s;
+    cin >> s;
+    suffix_array suf(s);
+    suf.build();
 
-    cout << suffix_array::longest_common_substring(s, t);
+    int q;
+    cin >> q;
+    vector<pair<int, int>> v(q);
+    for (int i = 0; i < q; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+        v[i].first--;
+        v[i].second--;
+    }
+
+    sort(v.begin(), v.end(), [&](pair<int, int> a, pair<int, int> b)
+         { return suf.comp(a, b); });
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i].first + 1 << " " << v[i].second + 1 << "\n";
 }
 int main()
 {
@@ -154,3 +203,6 @@ int main()
     while (TC--)
         solve();
 }
+
+// 10 3 11 5 13 8 0 16 4 12 7 15 6 14 9 2 1
+// 10 3 11 5 13 8 0 16 4 12 7 15 6 14 9 2 1
